9a870d79fd33f8d7978e824b628cf18b916d9843
Fernando Cueto <fernandoecueto@gmail.com>
2022-05-14 12:57:21 -0500

    set up to prod

--- app/Transformers/ZipcodeTransformer.php
+++ app/Transformers/ZipcodeTransformer.php
@@ -16,30 +16,36 @@ class ZipcodeTransformer extends TransformerAbstract
      */
     public function transform(Zipcode $zipcode)
     {
-        $settlements = array ();
-        foreach ($zipcode->settlements as $settlement) {
-            array_push($settlements, array(
-                'key' => (int)$settlement->key,
-                'name' => (string)strtoupper($settlement->name),
-                'zone_type' => (string)strtoupper($settlement->zone_type),
-                'settlement_type' => array('name' => (string)$settlement->settlementtype->name)
-                )
-            );
-        }
+        try {
+            $settlements = array ();
+            foreach ($zipcode->settlements as $settlement) {
+                array_push($settlements, array(
+                    'key' => (int)$settlement->key,
+                    'name' => (string)strtoupper($settlement->name),
+                    'zone_type' => (string)strtoupper($settlement->zone_type),
+                    'settlement_type' => array('name' => (string)$settlement->settlementtype->name)
+                    )
+                );
+            }
+    
+            return [
+                'zip_code' => (string)$zipcode->zip_code,
+                'locality' => (string)strtoupper($zipcode->locality),
+                'federal_entity' => [
+                    'key' => (int)$zipcode->municipality->federal_entity->key,
+                    'name' => (string)strtoupper($zipcode->municipality->federal_entity->name),
+                    'code' => isEmpty($zipcode->municipality->federal_entity->code) ? null : (string)$zipcode->municipality->federal_entity->code 
+                ],
+                'settlements' => $settlements,
+                'municipality' => [
+                    'key' => (int)$zipcode->municipality->key,
+                    'name' => (string)strtoupper($zipcode->municipality->name),
+                ]
+            ];
+        } catch (\Throwable $th) {
+            report($th);
 
-        return [
-            'zip_code' => (string)$zipcode->zip_code,
-            'locality' => (string)strtoupper($zipcode->locality),
-            'federal_entity' => [
-                'key' => (int)$zipcode->municipality->federal_entity->key,
-                'name' => (string)strtoupper($zipcode->municipality->federal_entity->name),
-                'code' => isEmpty($zipcode->municipality->federal_entity->code) ? null : (string)$zipcode->municipality->federal_entity->code 
-            ],
-            'settlements' => $settlements,
-            'municipality' => [
-                'key' => (int)$zipcode->municipality->key,
-                'name' => (string)strtoupper($zipcode->municipality->name),
-            ]
-        ];
+            return [];
+        }
     }
 }
